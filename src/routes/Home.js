92e8c423bdcead8d9da5fdcb9c54f5970aaef3ed
Hsource39 <source390@gmail.com>
2021-06-17 22:12:55 +0900

    #1.1

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,23 @@
 import React from "react";
-export default () => <h1>Home</h1>;
+import { gql, useQuery } from "@apollo/client"
+
+
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`
+
+export default () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    if(loading) {
+        return "Loading..."
+    }
+    if (data && data.movies) {
+        return data.movies.map(m => <hi>{m.id}<br/></hi>)
+    }
+return <h1>Home</h1>
+};
